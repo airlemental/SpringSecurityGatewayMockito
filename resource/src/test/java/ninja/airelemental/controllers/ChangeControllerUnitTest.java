ae20d08b893831d42678fb14225dd851625fa691
airlemental <airlemental@gmail.com>
2020-01-24 08:22:53 -0500

    Updated Resource Unit Tests

--- resource/src/test/java/ninja/airelemental/controllers/ChangeControllerUnitTest.java
+++ resource/src/test/java/ninja/airelemental/controllers/ChangeControllerUnitTest.java
@@ -13,6 +13,10 @@ import java.util.List;
 
 public class ChangeControllerUnitTest {
 
+    /**
+     * Section for GET endpoint calls to "/" using home()
+     */
+
     @Test // Happy
     public void homeReturnsBasicMessage() {
         // Expect and Initialize
@@ -48,55 +52,81 @@ public class ChangeControllerUnitTest {
         Assert.assertNotEquals(message, newMessage.getContent());
     }
 
+    /**
+     * Section for GET endpoint calls to "/changes" using changes()
+     */
+
     @Test // Happy
-    public void changesReturnsAListOfChanges() {
+    public void changesReturnsChangeListInsertedByUpdate() {
         // Expect and Initialize
-        String originalMessage = "Hello World";
-        List<Change> changes = new ArrayList<>();
-        changes.add(new Change("admin", "This is a change from setup."));
+        Message map = Mockito.mock(Message.class);
+        Principal principal = Mockito.mock(Authentication.class);
+        List<Change> mockChanges = new ArrayList<>();
+        mockChanges.add(new Change("admin", "This is a change from setup."));
 
         // Mock
-        ChangeController changeController = Mockito.mock(ChangeController.class);
 
         // Execute
         ChangeController realController2 = new ChangeController();
-//        realController2.changes("'user': 'admin'", "'content':'This is a change from the real Class.'");
+        String message = map.getContent();
+        List<Change> realChanges = realController2.changes();
+        realChanges.add(new Change(principal.getName(), message));
+        realController2.update(map, principal);
 
         // Verify
-//        Assert.assertEquals(changes, List from actual controller); // have to compare that each has 1 change
+        Assert.assertEquals(mockChanges.size(), realChanges.size()); // have to compare that each has 1 change
 
     }
 
     @Test // Un-Happy
     public void changesFailsToCreateOrConnect() {
         // Expect and Initialize
+        Message map = Mockito.mock(Message.class);
+        Principal principal = Mockito.mock(Authentication.class);
+        List<Change> mockChanges = new ArrayList<>();
+        mockChanges.add(new Change("admin", "This is a change from setup."));
 
         // Mock
-        ChangeController changeController = Mockito.mock(ChangeController.class);
 
         // Execute
+        ChangeController realController2 = new ChangeController();
+//        String message = map.getContent();
+        List<Change> realChanges = realController2.changes();
+//        realChanges.add(new Change(principal.getName(), message));  // Does not add any new content to the Change List.
+        realController2.update(map, principal);
 
         // Verify
+        Assert.assertNotEquals(mockChanges.size(), realChanges.size());
     }
+
+    /**
+     * Section for POST endpoint calls to "/" using update()
+     */
 
     @Test  // Happy
     public void updateChangesTheMessageContent() {
         // Expect and Initialize
-        String expectedMessage = "This is a change message.";
+        ChangeController withChangeList = new ChangeController();
         Message map = Mockito.mock(Message.class);
-        Principal principal = Mockito.mock(Authentication.class);
+        List<Change> setContent = withChangeList.changes();
+        setContent.add(new Change("admin", "Hello World"));
+        List<Change> content1 = withChangeList.changes();
+        content1.toArray();
+        content1.get(0);
+        System.out.println(content1);
 
         // Mock
-        ChangeController changeController = Mockito.mock(ChangeController.class);
-        Mockito.when(map.getContent()).thenReturn("This is a change message.");
-        Mockito.when(principal.getName()).thenReturn("Bob");
+
 
         // Execute
-        ChangeController realController4 = new ChangeController();
-        Message newChange = realController4.update(map, principal);
+        setContent.add(new Change("admin", "This content is changed using update."));
+        List<Change> content2 = withChangeList.changes();
+        content2.toArray();
+        content2.get(0);
+        System.out.println(content2);
 
         // Verify
-        Assert.assertEquals(expectedMessage, newChange.getContent());
+        Assert.assertNotEquals(content1 , content2);
     }
 
     @Test  // Happy
